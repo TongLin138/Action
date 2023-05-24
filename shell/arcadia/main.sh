5f5df318a2df08cbb60f7b22ce98b55eb2a79027
SuperManito <68613938+SuperManito@users.noreply.github.com>
2023-05-25 04:17:18 +0800

    feat: 适配快捷导入脚本配置命令入口

--- shell/arcadia/main.sh
+++ shell/arcadia/main.sh
@@ -7,71 +7,6 @@ case $# in
     import core/help
     Help $ContrlCmd
     ;;
-1)
-    Output_Command_Error 1 # 命令错误
-    ;;
-2)
-    case $1 in
-    service)
-        case $2 in
-        start | stop | info | respwd)
-            import arcadia/service
-            Service_Control $2
-            ;;
-        *)
-            Output_Command_Error 1 # 命令错误
-            ;;
-        esac
-        ;;
-    tgbot)
-        case $2 in
-        start | stop | logs | update)
-            import arcadia/tgbot
-            TGBot_Control $2
-            ;;
-        *)
-            Output_Command_Error 1 # 命令错误
-            ;;
-        esac
-        ;;
-    server)
-        case $2 in
-        status)
-            import arcadia/service
-            Server_Status
-            ;;
-        *)
-            Output_Command_Error 1 # 命令错误
-            ;;
-        esac
-        ;;
-    env)
-        case $2 in
-        install | repairs)
-            import arcadia/env
-            Environment_Deployment $2
-            ;;
-        *)
-            Output_Command_Error 1 # 命令错误
-            ;;
-        esac
-        ;;
-    check)
-        case $2 in
-        files)
-            import arcadia/env
-            Check_Files
-            ;;
-        *)
-            Output_Command_Error 1 # 命令错误
-            ;;
-        esac
-        ;;
-    *)
-        Output_Command_Error 1 # 命令错误
-        ;;
-    esac
-    ;;
 *)
     case $1 in
     repo)
@@ -83,7 +18,76 @@ case $# in
         Add_Raw "$@"
         ;;
     *)
-        Output_Command_Error 2 # 命令过多
+        case $# in
+        1)
+            Output_Command_Error 1 # 命令错误
+            ;;
+        2)
+            case $1 in
+            service)
+                case $2 in
+                start | stop | info | respwd)
+                    import arcadia/service
+                    Service_Control $2
+                    ;;
+                *)
+                    Output_Command_Error 1 # 命令错误
+                    ;;
+                esac
+                ;;
+            tgbot)
+                case $2 in
+                start | stop | logs | update)
+                    import arcadia/tgbot
+                    TGBot_Control $2
+                    ;;
+                *)
+                    Output_Command_Error 1 # 命令错误
+                    ;;
+                esac
+                ;;
+            server)
+                case $2 in
+                status)
+                    import arcadia/service
+                    Server_Status
+                    ;;
+                *)
+                    Output_Command_Error 1 # 命令错误
+                    ;;
+                esac
+                ;;
+            env)
+                case $2 in
+                install | repairs)
+                    import arcadia/env
+                    Environment_Deployment $2
+                    ;;
+                *)
+                    Output_Command_Error 1 # 命令错误
+                    ;;
+                esac
+                ;;
+            check)
+                case $2 in
+                files)
+                    import arcadia/env
+                    Check_Files
+                    ;;
+                *)
+                    Output_Command_Error 1 # 命令错误
+                    ;;
+                esac
+                ;;
+            *)
+                Output_Command_Error 1 # 命令错误
+                ;;
+            esac
+            ;;
+        *)
+            Output_Command_Error 2 # 命令过多
+            ;;
+        esac
         ;;
     esac
     ;;
--- shell/arcadia/raw.sh
+++ shell/arcadia/raw.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-## Modified: 2023-05-23
+## Modified: 2023-05-25
 
 function Add_Raw() {
-    echo '{ "name": "abc", url: "abc", cronSettings: { updateTaskList: true }' | jq
+    # 定义临时文件
+    local tmp_file="${ARCADIA_DIR}/log/tmp/raw.yml"
+    # 生成配置文件模板
+    function CreateTemplate() {
+        echo '{ "name": "", "url": "", "cronSettings": { "updateTaskList": true } }' | jq | yq -y >$tmp_file
+        # 插入缩进空格
+        local LineSum="$(cat $tmp_file | grep "" -c)"
+        for ((i = 1; i <= $LineSum; i++)) do
+            [ $i -eq 1 ] && sed -i "1s/^/  - /g" $tmp_file || sed -i "${i}s/^/  - /g" $tmp_file
+        done
+    }
+
+    CreateTemplate
+    [ -f $tmp_file ] && rm -rf $tmp_file
 }
