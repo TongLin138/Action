eee62c0400a2d76cb32e565842fd91773f399227
SuperManito <68613938+SuperManito@users.noreply.github.com>
2023-06-15 19:42:22 +0800

    style: 更新日志

--- web/core/cron/core.js
+++ web/core/cron/core.js
@@ -29,7 +29,7 @@ function cronInit(){
             }
             try {
                 engine.setTask(task.id, task.cron, () => onCron(task))
-                logger.log('设置定时任务成功', task.id.split("T_")[1])
+                // logger.log('设置定时任务成功', task.id.split("T_")[1])
             } catch (e) {
                 logger.log('设置定时任务失败', task.id.split("T_")[1], `=> ${task.cron} ${e.message || e}`)
             }
