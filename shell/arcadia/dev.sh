f518b5497f7298a3e0b3c7aad003751a3e2a23a1
SuperManito <68613938+SuperManito@users.noreply.github.com>
2023-06-15 18:24:20 +0800

    fix: bug

--- shell/arcadia/dev.sh
+++ shell/arcadia/dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-## Modified: 2023-06-13
+## Modified: 2023-06-16
 
 function change_dev_version() {
     git remote -v | grep -q 'arcadia_github'
@@ -8,7 +8,7 @@ function change_dev_version() {
         echo -e "\n$COMPLETE 已切换回用户版本\n"
     else
         cat /root/.ssh/config | grep -q 'arcadia_github'
-        [ $? -ne 0 ] && echo -e "Host arcadia_github\n\tHostname github.com\n\tIdentityFile=/root/.ssh/arcadia\n" >/root/.ssh/config
+        [ $? -ne 0 ] && echo -e "Host arcadia_github\n\tHostname github.com\n\tIdentityFile=/root/.ssh/arcadia\n" >>/root/.ssh/config
         git remote set-url origin git@arcadia_github:SuperManito/ArcadiaBase.git
         echo -e "\n$COMPLETE 已为您切换至开发版本，感谢参与测试\n"
     fi
